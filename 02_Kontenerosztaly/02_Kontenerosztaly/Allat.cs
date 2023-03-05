a392ee2592004c1c2226e097b324762d7b1dda14
O48WRX <kardyy81@gmail.com>
2023-03-05 16:17:13 +0100

    Container Class and Indexer

---
+++ 02_Kontenerosztaly/02_Kontenerosztaly/Allat.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02_Kontenerosztaly
+{
+    public class Allat
+    {
+        // Mezők
+        string nev;
+        string faj;
+        Nem nem;
+
+
+        // Property-k
+        public string Nev 
+        { 
+            get { return nev; }
+            set { nev = value; }
+        }
+
+        public string Faj
+        {
+            get { return faj; }
+            set { faj = value; }
+        }
+
+        public Nem Nem
+        {
+            get { return nem; }
+            set { nem = value; }
+        }
+
+        // Konstruktor
+        public Allat(string nev, string faj, Nem nem)
+        {
+            this.Nev = nev;
+            this.Faj = faj;
+            this.Nem = nem;
+        }
+    }
+}
---
+++ 02_Kontenerosztaly/02_Kontenerosztaly/Allatkert.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02_Kontenerosztaly
+{
+    public class Allatkert
+    {
+        string nev;
+        int jegyar;
+        int maxfo;
+        List<Allat> Allatok = new List<Allat>();
+
+        public string Nev { get { return nev; } set { nev = value; } }
+        public int Jegyar { get { return jegyar; } set { jegyar = value; } }
+        public int Maxfo { get { return maxfo; } set { maxfo = value; } }
+
+        public Allatkert(string nev, int jegyar, int maxfo)
+        {
+            this.Nev = nev;
+            this.Jegyar = jegyar;
+            this.Maxfo = maxfo;
+        }
+
+        // Indexer
+        public Allat this[int i]
+        {
+            get { return this.Allatok[i]; }
+        }
+    }
+}
