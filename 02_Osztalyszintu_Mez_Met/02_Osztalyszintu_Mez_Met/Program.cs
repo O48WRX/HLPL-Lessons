d3e4fef8dfbdd1c1bad14e1cc19dba8408a7135f
O48WRX <kardyy81@gmail.com>
2023-03-06 09:55:19 +0100

    Class level methods and fields lesson added

---
+++ 02_Osztalyszintu_Mez_Met/02_Osztalyszintu_Mez_Met/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02_Osztalyszintu_Mez_Met
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(StopperOra.GetPeldanyokSzama());
+            StopperOra ora1 = new StopperOra();
+            StopperOra ora2 = new StopperOra();
+            StopperOra ora3 = new StopperOra();
+            StopperOra ora4 = new StopperOra();
+
+            Console.WriteLine(StopperOra.peldanyokszama);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ 02_Osztalyszintu_Mez_Met/02_Osztalyszintu_Mez_Met/StopperOra.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02_Osztalyszintu_Mez_Met
+{
+    public class StopperOra
+    {
+        public static int peldanyokszama = 0;
+
+        public StopperOra()
+        {
+            StopperOra.peldanyokszama++;
+        }
+
+        public static int GetPeldanyokSzama()
+        {
+            return peldanyokszama;
+        }
+    }
+}
