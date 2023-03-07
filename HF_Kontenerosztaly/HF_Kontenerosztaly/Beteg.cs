3acde8ee3a72c0e0cc91bde8e08e8b93d8db0028
O48WRX <kardyy81@gmail.com>
2023-03-07 21:17:16 +0100

    Container class homework added

---
+++ HF_Kontenerosztaly/HF_Kontenerosztaly/Beteg.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HF_Kontenerosztaly
+{
+    public class Beteg
+    {
+		private string nev;
+		private DateTime szuldatum;
+		private string betegazonosito;
+
+		public Beteg(string nev, DateTime szuldatum, string betegazonosito)
+		{
+			this.Nev = nev;
+			this.Szuldatum = szuldatum;
+			this.Betegazonosító = betegazonosito;
+		}
+
+		public string Betegazonosító
+		{
+			get { return betegazonosito; }
+			set { betegazonosito = value; }
+		}
+
+
+		public DateTime Szuldatum
+		{
+			get { return szuldatum; }
+			set { szuldatum = value; }
+		}
+
+
+		public string Nev
+		{
+			get { return nev; }
+			set { nev = value; }
+		}
+
+        public override string ToString()
+        {
+			return String.Format("Nev = {0}, Születési dátum = {1}, betegazonosító = {2}",this.Nev, this.Szuldatum, this.Betegazonosító);
+        }
+    }
+}
---
+++ HF_Kontenerosztaly/HF_Kontenerosztaly/Korhaz.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HF_Kontenerosztaly
+{
+    public class Korhaz
+    {
+		private string nev;
+		private int agyakszama;
+		List<Beteg> betegek;
+
+		public Korhaz(string nev, int tarolokapacitas)
+		{
+			this.Nev = nev;
+			this.Agyakszama = tarolokapacitas;
+			betegek = new List<Beteg>();
+		}
+
+		public int Agyakszama
+		{
+			get { return agyakszama; }
+			set { agyakszama = value; }
+		}
+
+
+		public string Nev
+		{
+			get { return nev; }
+			set { nev = value; }
+		}
+
+		// Indexer
+		public Beteg this[int i]
+		{
+			get { return betegek[i];}
+		}
+
+		public void addBeteg(Beteg beteg)
+		{
+			// Leteszteljük, hogy benne van-e már a listában a betegünk
+			// Nem akarjuk kétszer felvenni ugyanazt a beteget.
+			if (!betegek.Contains(beteg))
+			{
+				betegek.Add(beteg);
+			}
+		}
+
+		public void removeBeteg(Beteg beteg)
+		{
+			if (!betegek.Contains(beteg))
+			{
+				Console.WriteLine("Nem létezik ilyen beteg!");
+			}
+			// Megszerezzük az objektumunk indexét, mivel tudjuk, hogy benne van a listában.
+			int index = betegek.IndexOf(beteg);
+			// Index alapján kitöröljük a listából.
+			betegek.RemoveAt(index);
+		}
+
+	}
+}
