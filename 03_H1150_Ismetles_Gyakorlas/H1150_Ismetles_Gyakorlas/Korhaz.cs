164037479fa97fd19fcb37a514824219d490e42b
O48WRX <kardyy81@gmail.com>
2023-03-13 21:17:28 +0100

    Classes & Container classes exercise

---
+++ 03_H1150_Ismetles_Gyakorlas/H1150_Ismetles_Gyakorlas/Korhaz.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace H1150_Ismetles_Gyakorlas
+{
+	// Konténer osztály
+    public class Korhaz
+    {
+		private string nev;
+		// Konténer listánk
+		List<Beteg> Beteglista;
+
+		public string Nev
+		{
+			get { return nev; }
+			set { nev = value; }
+		}
+
+		private int maxfo;
+
+		public int Maxfo
+		{
+			get { return maxfo; }
+			set { maxfo = value; }
+		}
+
+		public Korhaz(string nev, int maxfo)
+		{
+			this.Nev = nev;
+			this.Maxfo = maxfo;
+			Beteglista = new List<Beteg>();
+		}
+
+		public Beteg this[int index]
+		{
+			get { return Beteglista[index]; }
+			set { Beteglista[index] = value; }
+		}
+
+		public void Hozzaad(Beteg beteg)
+		{
+			Beteglista.Add(beteg);
+		}
+
+		public void Kivesz(Beteg beteg)
+		{
+			// Ha nincs benne a listában akkor nincs mit kivenni
+			// Ekkor kilépünk
+			if (!Beteglista.Contains(beteg))
+			{
+				return;
+			}
+
+			int betegindex = Beteglista.IndexOf(beteg);
+
+			Beteglista.RemoveAt(betegindex);
+
+			/*
+			 * Úgy is meg lehet oldani, hogy nem indexet keresünk
+			 * 
+			 * Beteglista.Remove(beteg)
+			 * 
+			 */
+		}
+	}
+}
---
+++ 03_H1150_Ismetles_Gyakorlas/H1150_Ismetles_Gyakorlas/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace H1150_Ismetles_Gyakorlas
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Diak diak1 = new Diak("Péter", "POIAISNOI", 3);
+            Console.WriteLine(diak1);
+
+            // Konténer osztály teszt
+
+            Beteg b1 = new Beteg("Sándor", "AB82", 2);
+            Beteg b2 = new Beteg("Cintia", "kl93", 1);
+
+            Korhaz korhaz1 = new Korhaz("Markhot Ferenc Kórház", 10);
+
+            korhaz1.Hozzaad(b1);
+            korhaz1.Hozzaad(b2);
+
+            Console.WriteLine(korhaz1[0]);
+            Console.WriteLine(korhaz1[1]);
+
+            korhaz1.Kivesz(b2);
+
+            Console.ReadKey();
+        }
+    }
+}
